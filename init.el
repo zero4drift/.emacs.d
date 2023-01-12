6096982c4d1fb60aef6bc6b710bca8b1cff3b31a
zero4drift <fang0052@e.ntu.edu.sg>
2023-01-12 12:53:31 +0800

    Delete anki related phrases and files add the original ending phrase

--- init.el
+++ init.el
@@ -169,8 +169,6 @@
 
 ;; encoding system
 (setq-default language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(setq-default buffer-file-coding-system 'utf-8)
 
 ;; on-the-fly indentation
 (electric-indent-mode t)
@@ -207,10 +205,6 @@
   (deft-use-filter-string-for-filename nil)
   (deft-org-mode-title-prefix nil)
   (deft-use-filename-as-title t))
-
-;; anki
-(use-package anki-connect)
-(use-package anki-editor)
 
 ;; iedit
 (use-package iedit
@@ -650,3 +644,6 @@
 ;; end blog
 ;; end use-packages
 
+
+(provide 'init)
+;;; init.el ends here
